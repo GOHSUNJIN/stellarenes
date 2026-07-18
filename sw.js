5982a7277326fb7156daa6adbf9fbe2298a0dbd6
Sun Jin <gohsunjin@gmail.com>
2026-07-19 00:22:46 +0800

    update messages

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Stellarenes service worker — bump VERSION on every deploy to force reload
-const VERSION = '2026-07-18-1009';
+const VERSION = '2026-07-18-1622';
 
 self.addEventListener('install', () => self.skipWaiting());
 
